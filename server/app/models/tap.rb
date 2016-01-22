0d3051c615da902a2acd7eddc1b42c7bfca1874a
João Gradim <joao.gradim@gmail.com>
2016-01-22 17:28:20 +0000

    Taps resource and controller

---
+++ server/app/models/tap.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: taps
+#
+#  id         :integer          not null, primary key
+#  brand      :string
+#  name       :string
+#  style      :string
+#  abv        :float
+#  country    :string
+#  city       :string
+#  half_price :float
+#  full_price :float
+#  position   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Tap < ActiveRecord::Base
+  include RankedModel
+  ranks :position
+end
