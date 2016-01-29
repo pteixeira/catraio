da04988149a3f7ed97dd4692428edafc4ef30301
pteixeira <pedro.ap.teixeira@gmail.com>
2016-01-29 16:42:34 +0000

    Fixed links, fixed texts, fixed classes

--- client/src/components/app/footer.js
+++ client/src/components/app/footer.js
@@ -1,14 +1,15 @@
 import "./footer.styl";
 
 import React from "react";
+import { Link } from "react-router";
 import { translate } from "react-i18next/lib";
 
 const MENU_ITEMS = [
   "photos", "events", "about", "contact", "beers"
 ];
 
-class Header extends React.Component {
-  static displayName = "Header";
+class Footer extends React.Component {
+  static displayName = "Footer";
 
   render() {
     const { t } = this.props;
@@ -16,10 +17,11 @@ class Header extends React.Component {
     return (
       <footer className="Footer">
         <a href="https://www.facebook.com/catraiobeershop/">Facebook</a>
-        <a href="https://untappd.com/venue/2524942">Untappd1</a>
+        <a href="https://untappd.com/venue/2524942">Untappd</a>
+        <Link to="/about">{t("menu:about")}</Link>
       </footer>
     );
   }
 }
 
-export default translate(["header", "menu"])(Header);
+export default translate(["header", "menu"])(Footer);
