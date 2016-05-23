11b0b0a69ac534fcdf62ee71503373184034cf8d
João Gradim <joao.gradim@gmail.com>
2016-05-23 15:08:25 +0100

    Initial redesigned styles

--- client/src/components/app/header.js
+++ client/src/components/app/header.js
@@ -14,24 +14,23 @@ class Header extends React.Component {
 
   render() {
     const { t } = this.props;
+
     return (
       <header className="Header">
 
-        <div className="Header-content">
-          <Link to="/">
-            <h1 className="logo">Catraio</h1>
-          </Link>
+        <Link className="logo" to="/">
+          <h1>Catraio</h1>
+        </Link>
 
-          <ul className="Header-menu">
-            {map(MENU_ITEMS, (item) => {
-              return (
-                <li className="Header-menu-item" key={`header-link-${item}`}>
-                  <Link to={item} activeClassName="active">{t(`menu:${item}`)}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <ul className="Header-menu">
+          {map(MENU_ITEMS, (item) => {
+            return (
+              <li className="Header-menu-item" key={`header-link-${item}`}>
+                <Link to={item} activeClassName="active">{t(`menu:${item}`)}</Link>
+              </li>
+            );
+          })}
+        </ul>
 
       </header>
     );
