22a9378dfd706c8e081222fc5780304f7bd2b39b
João Gradim <joao.gradim@gmail.com>
2017-05-10 20:21:31 +0100

    [REFACTOR] Remove old components

--- client/src/router.js
+++ client/src/router.js
@@ -2,19 +2,11 @@ import React from "react";
 import { Router, Route, browserHistory } from "react-router";
 
 // Top-level components
-import Admin from "app-components/admin";
 import Disclaimer from "app-components/disclaimer";
 import App from "app-components/app";
 
-// import Login from "app-components/login";
-// import Photos from "app-components/photos";
-// import Beers from "app-components/beers";
-// import Events from "app-components/events";
-// import About from "app-components/about";
-
 const router = (
   <Router history={browserHistory}>
-    <Route path="/admin" component={Admin} />
     <Route path="/disclaimer" component={Disclaimer} />
     <Route path="/" component={App} />
   </Router>
